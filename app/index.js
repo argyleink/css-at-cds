2adb9e113f41221491b0529bc1fbeb51a792e504
Adam Argyle <atom@argyleink.com>
2019-11-06 13:50:39 -0800

    moving some files, added svg

--- app/index.js
+++ app/index.js
@@ -1,1 +1,8 @@
 console.log('CSS FTW!')
+
+const {matches:prefers_reduced} = 
+  window.matchMedia('(prefers-reduced-motion:reduce)')
+
+if (prefers_reduced)
+  document.querySelectorAll('video').forEach(node =>
+    node.pause())
